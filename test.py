3487670ed0627113fcc19f02a124a0db59543862
alexgarciam <agmarchena@gmail.com>
2016-11-26 19:36:14 +0100

    anges in the file message on led change status

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ GPIO.setup(11, GPIO.OUT)
 
 while True:
     GPIO.output(11, True)
-    print "LED ON"
+    print "LED ON!"
     time.sleep(2)
     GPIO.output(11, False)
-    print "LED OFF"
+    print "LED OFF!"
     time.sleep(2)
