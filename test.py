116552829e22f58982d6b7ef4adbf84bcaafb841
alexgarciam <agmarchena@gmail.com>
2016-11-26 19:28:23 +0100

    primera versión de iluminación de leds en raspaberry con python

---
+++ test.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+while True:
+    GPIO.output(11, True)
+    print "LED ON"
+    time.sleep(2)
+    GPIO.output(11, False)
+    print "LED OFF"
+    time.sleep(2)
